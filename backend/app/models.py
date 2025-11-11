d8445121aaa69b17e4ac7da20e7a485e9646a6bb
JayXie9499 <tsum950210@gmail.com>
2025-11-12 02:57:12 +0800

    feat: Setup database connection and models for User and Post

---
+++ backend/app/models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+from .database import Base
+
+
+class User(Base):
+	__tablename__ = "users"
+
+	id = Column(Integer, primary_key=True, index=True)
+	name = Column(String(30), unique=True, nullable=False)
+	hashed_pwd = Column(Text, nullable=False)
+	posts = relationship("Post", back_populates="owner")
+
+
+class Post(Base):
+	__tablename__ = "posts"
+
+	id = Column(Integer, primary_key=True, index=True)
+	title = Column(String(100), nullable=False)
+	content = Column(Text, nullable=False)
+	created_at = Column(DateTime(timezone=True), server_default=func.now())
+	owner_id = Column(Integer, ForeignKey("users.id"))
+	owner = relationship("User", back_populates="posts")
